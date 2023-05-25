ef35689fca327805b78c48132635bac14236e5c7
Bror Brurberg <bror@hey.com>
2023-05-25 20:52:33 +0200

    Initial Commit

---
+++ ResponseMocker/ResponseMockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResponseMockerApp.swift
+//  ResponseMocker
+//
+//  Created by Bror2 on 25/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ResponseMockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
